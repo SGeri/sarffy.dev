c8c4c87b0cbbd3e8ac96d81912c1f67758f204c7
SGeri <sarffygeri@gmail.com>
2023-01-24 20:54:49 +0100

    add font subsets to be able use hungarian characters

--- web/src/fonts/index.ts
+++ web/src/fonts/index.ts
@@ -3,4 +3,7 @@ import { Fira_Code, Noto_Sans } from "@next/font/google";
 export const FiraCodeFont = Fira_Code({ weight: "400" });
 export const FiraCodeBoldFont = Fira_Code({ weight: "600" });
 export const NotoSansFont = Noto_Sans({ weight: "400" });
-export const NotoSansBoldFont = Noto_Sans({ weight: "700" });
+export const NotoSansBoldFont = Noto_Sans({
+  subsets: ["latin-ext"],
+  weight: "700",
+});
